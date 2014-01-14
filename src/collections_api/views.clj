cde30c977a5b26dbf1de01b16d47d8b55763ae89
Jesse Nelson <jnels124@msudenver.edu>
2014-01-14 00:39:52 -0700

    Added file to handle views... just in case

---
+++ src/collections_api/views.clj
@@ -0,0 +1,9 @@
+(ns collections-api.views
+  (:use hiccup.page hiccup.element))
+
+;(defn index-page [] "Nice and easy!")
+(defn index-page []
+  (html5
+   [:html
+    [:head]
+    [:body "Bla"]]))
