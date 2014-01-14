13dac7dab26355ce0a627255e48c9f011937b335
Jesse Nelson <jnels124@msudenver.edu>
2014-01-14 00:36:21 -0700

    Added route to handle barcode and retrieve data

--- src/collections_api/handler.clj
+++ src/collections_api/handler.clj
@@ -1,12 +1,22 @@
 (ns collections-api.handler
-  (:use compojure.core)
+  (:use compojure.core
+        ring.middleware.json
+        ring.util.response)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [collections-api.views :as views]
+            [clojure.java.io :as io]))
+
+(defn- get-ean-map
+  "Retrieves initial data from EAN database"
+  [id]
+  (slurp (str "http://eandata.com/feed/?v=3&keycode=027F74ED52886617&mode=json&find="id)))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (views/index-page))
+  (GET "/:id" [id] (response (get-ean-map id)))
   (route/resources "/")
   (route/not-found "Not Found"))
 
-(def app
-  (handler/site app-routes))
+;(def app(handler/site app-routes))
+(def app (wrap-json-response app-routes))
